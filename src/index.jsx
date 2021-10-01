5bd8adf2c8cf1d526105ea367f202a536b6b234f
felipebigarellidantasdacosta <felipe.costa@g4tech.com.br>
2021-10-01 13:40:56 -0300

    Configurando Ambiente

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { render } from 'react-dom';
+
+import { App } from './App';
+
+/**
+ * render(2 parametros): 
+ * 1 - o quer quer que seja exibido em tela;
+ * 2 - dentro de qual elemento quer renderizar a informação.
+ */
+render(<App />, document.getElementById('root'));
