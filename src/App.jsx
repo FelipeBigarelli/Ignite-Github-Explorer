b34cbba5f4ac4b897953c566a709d2c9a873e808
felipebigarellidantasdacosta <felipe.costa@g4tech.com.br>
2021-10-04 13:20:01 -0300

    Configured Webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React from 'react';
+
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss'
 
-import React from 'react';
-import { RepositoryList } from './components/RepositoryList';
 export function App() {
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -19,18 +20,27 @@ module.exports = {
         static: {
           directory: path.resolve(__dirname, 'public')
         },
+        hot: true,
     },
     plugins: [ // injetar o js no html para não precisar importar no index.html por meio do <script>
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html') // arquivo de template para gerar o html
         })
-    ],
+    ].filter(Boolean),
     module: { // como a aplicação irá se comportar com vários tipos de arquivos
         rules: [
             {
                 test: /\.jsx$/, // true | false: é js ou não
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
