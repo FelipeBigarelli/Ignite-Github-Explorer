1a31fdb24d8ed71c2b2011d7433fa0e64b737caf
felipebigarellidantasdacosta <felipe.costa@g4tech.com.br>
2021-10-04 13:58:51 -0300

    List Github API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 
 import './styles/global.scss'
@@ -9,7 +8,6 @@ export function App() {
     return (
         <>
             <RepositoryList />
-            <Counter />
         </>
     )
 }
